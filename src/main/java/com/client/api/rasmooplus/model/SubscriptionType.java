403a8675a35aacd4b858342dd12a9b3a46d03777
danplima <danplima25@gmail.com>
2025-10-24 19:23:19 -0400

    feat: criando as entidades do projeto.
    Utiliando lombok para aceletar o desenvolvimento.

---
+++ src/main/java/com/client/api/rasmooplus/model/SubscriptionType.java
@@ -0,0 +1,33 @@
+package com.client.api.rasmooplus.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "subscriptions_type")
+public class SubscriptionType implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "subscriptions_type_id")
+    private Long id;
+
+    private String name;
+
+    @Column(name = "access_months")
+    private Long acessMonth;
+
+    private BigDecimal price;
+
+    @Column(name = "product_key")
+    private String productKey;
+}
